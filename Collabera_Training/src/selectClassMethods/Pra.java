9457624f5f5fad937927b4efdd3d073258dbf767
123Arpitha <arpithaggowda3108@gmail.com>
2022-06-09 13:28:48 +0530

    First push

---
+++ Collabera_Training/src/selectClassMethods/Pra.java
@@ -0,0 +1,27 @@
+package selectClassMethods;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Pra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("https://www.facebook.com/signup");
+		WebElement date = driver.findElement(By.id("day"));
+		Select select=new Select(date);
+		select.selectByValue("5");
+		
+	}
+
+}
